75894e3e286f81fde567b3e386077953cbb030ed
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-21 23:57:21 +0700

    8.1 Update EventCard.vue to receive event obj via Props

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-// defineProps<{
-//   msg: string
-// }>()
-const event = ref({
-  id: 5928101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at our adoption event.',
-  location: 'Meow Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee',
-})
+import type { Event } from '@/types'
+defineProps<{
+  event: Event
+}>()
+
+// const event = ref({
+//   id: 5928101,
+//   category: 'animal welfare',
+//   title: 'Cat Adoption Day',
+//   description: 'Find your new feline friend at our adoption event.',
+//   location: 'Meow Town',
+//   date: 'January 28, 2022',
+//   time: '12:00',
+//   petsAllowed: true,
+//   organizer: 'Kat Laydee',
+// })
 </script>
 
 <template>

fc28906934929b2d2a1f39d69d8160cfaaded8bd
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-25 20:28:44 +0700

    1.5 Update EventCard.vue

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -4,27 +4,15 @@ import type { Event } from '@/types'
 defineProps<{
   event: Event
 }>()
-
-// const event = ref({
-//   id: 5928101,
-//   category: 'animal welfare',
-//   title: 'Cat Adoption Day',
-//   description: 'Find your new feline friend at our adoption event.',
-//   location: 'Meow Town',
-//   date: 'January 28, 2022',
-//   time: '12:00',
-//   petsAllowed: true,
-//   organizer: 'Kat Laydee',
-// })
 </script>
 
 <template>
-  <div class="event-class">
+  <RouterLink to="/event/5928101">
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
-  </div>
+  </RouterLink>
 </template>
 
 <style scoped>

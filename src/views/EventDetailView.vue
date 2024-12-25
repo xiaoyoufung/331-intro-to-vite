4cc97ddb334d46d51f0b5ee5d1af7b7eab794635
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-25 20:22:53 +0700

    1.3 Add id to EventDetailView.vue

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import Event from '@/types/Event'
+import EventService from '@/services/EventService'
 const event = ref<Event>(null)
+const id = ref<number>(5928101)
 onMounted(() => {
-  //  fetch event (by id) and set local event data
+  EventService.getEvent(id.value)
+    .then((response) => {
+      event.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
 })
 </script>
 <template>

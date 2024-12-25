8959de3e2babad22720ac5e942b5646a2c5e7e6c
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-25 19:37:40 +0700

    fix 16.3 Adding student view

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Event</RouterLink> |
-          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/">Event</RouterLink> | <RouterLink to="/about">About</RouterLink> |
+          <RouterLink to="/student">Student</RouterLink>
         </nav>
       </div>
     </header>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
+import StudentView from '@/views/StudentView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/student',
+      name: 'students-list-view',
+      component: StudentView,
     },
   ],
 })

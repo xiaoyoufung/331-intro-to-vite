9fdfa7e9d8963bf00fcdbaebf269d7773d60e238
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-25 20:40:05 +0700

    1.8 add class "event-link" to EventCard.vue

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <RouterLink :to="{ name: 'event-detail-view', params: { id: event.id } }">
+  <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
@@ -26,5 +26,10 @@ defineProps<{
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>

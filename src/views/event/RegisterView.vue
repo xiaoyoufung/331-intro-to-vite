8d98354cd05bd7fec9b01e588821ee0e9442da54
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-27 13:57:15 +0700

    4.2 Use useRouter function to crate the router

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useRouter } from 'vue-router'
 
 const props = defineProps<{
   event: Event
@@ -8,11 +9,12 @@ const props = defineProps<{
 }>()
 
 const { event } = toRefs(props)
+const router = useRouter()
 
 const register = () => {
   // If the register API is call successful
   // Pull back to the event details view
-  console.log('Register event')
+  router.push({ name: 'event-detail-view' })
 }
 </script>
 <template>

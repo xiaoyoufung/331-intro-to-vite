b25ee3042d995ec02adf9b9bc0cf58ddd8284a85
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-27 13:41:25 +0700

    3.2 Update the code to use nested route

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import StudentView from '@/views/StudentView.vue'
 import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,21 +21,29 @@ const router = createRouter({
     },
     {
       path: '/event/:id',
-      name: 'event-detail-view',
-      component: EventDetailView,
+      name: 'event-layout-view',
+      component: EventLayoutView,
       props: true,
-    },
-    {
-      path: '/event/:id/register',
-      name: 'event-register-view',
-      component: EventRegisterView,
-      props: true,
-    },
-    {
-      path: '/event/:id/edit',
-      name: 'event-edit-view',
-      component: EventEditView,
-      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true,
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true,
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true,
+        },
+      ],
     },
     {
       path: '/about',
--- src/views/event/DetailView.vue
+++ src/views/event/DetailView.vue
@@ -1,33 +1,14 @@
 <script setup lang="ts">
-import { ref, onMounted, defineProps } from 'vue'
+import { toRefs, defineProps } from 'vue'
 import { type Event } from '@/types'
-import EventService from '@/services/EventService'
-const event = ref<Event | null>(null)
-const props = defineProps({
-  id: {
-    type: String,
-    required: true,
-  },
-})
-onMounted(() => {
-  EventService.getEvent(parseInt(props.id))
-    .then((response) => {
-      event.value = response.data
-    })
-    .catch((error) => {
-      console.error('There was an error!', error)
-    })
-})
+
+const props = defineProps<{
+  event: Event
+}>()
+
+const { event } = toRefs(props)
 </script>
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <nav>
-      <router-link :to="{ name: 'event-detail-view' }">Details</router-link> |
-      <router-link :to="{ name: 'event-register-view' }">Register</router-link> |
-      <router-link :to="{ name: 'event-edit-view' }">Edit</router-link>
-    </nav>
-    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
-    <p>{{ event.description }}</p>
-  </div>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
 </template>

c8e691e59d240dca28928da8a136798dcbebd0da
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-28 13:37:29 +0700

    1.5 Update register function in event/RegisterView.vue

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -2,6 +2,7 @@
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
 import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
   event: Event
@@ -10,11 +11,14 @@ const props = defineProps<{
 
 const { event } = toRefs(props)
 const router = useRouter()
+const store = useMessageStore()
 
 const register = () => {
-  // If the register API is call successful
-  // Pull back to the event details view
-  router.push({ name: 'event-detail-view' })
+  store.updateMessage('You are successfully registered for ' + props.event.title)
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { id: props.id } })
 }
 </script>
 <template>

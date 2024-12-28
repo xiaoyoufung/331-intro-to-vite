8076457ebd6977d7bd3195b0f2e7cd0ee007a9a1
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-28 21:29:00 +0700

    2.3 Set progress bar to global route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
+import nProgress from 'nprogress'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -74,6 +75,12 @@ const router = createRouter({
       component: NotFoundView,
     },
   ],
+})
+router.beforeEach(() => {
+  nProgress.start()
+})
+router.afterEach(() => {
+  nProgress.done()
 })
 
 export default router

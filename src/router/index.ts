771d92ff999cc1f3bd0856893703985a955573b4
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-28 21:34:18 +0700

    3.1 Update router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,6 +27,9 @@ const router = createRouter({
       name: 'event-layout-view',
       component: EventLayoutView,
       props: true,
+      beforeEnter: (to) => {
+        // put API call here
+      },
       children: [
         {
           path: '',

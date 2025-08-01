f3296717b790cc75d2e09cef6a52d7835d3459b9
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-08-01 10:58:26 +0700

    add

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics';
+
+import 'nprogress/nprogress.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,6 +13,7 @@ import 'nprogress/nprogress.css'
 const pinia = createPinia()
 const app = createApp(App)
 
+inject(); // Initialize Vercel Analytics
 app.use(pinia)
 app.use(router)
 
